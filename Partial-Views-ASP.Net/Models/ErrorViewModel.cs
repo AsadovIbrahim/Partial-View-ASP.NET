6f7b0e45e3eb43b3c2a7bd1b79996da78a2f026c
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-05-04 18:53:12 +0400

    commit

---
+++ Partial-Views-ASP.Net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Partial_Views_ASP.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
